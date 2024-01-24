4d152de2787bf65f890ce305c2c81198659bffef
Daniil Kaminskyi <98901710+whit33th@users.noreply.github.com>
2024-01-24 21:41:14 +0100

    first commit

---
+++ src/test/java/com/whit33th/azure/redshop/RedShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whit33th.azure.redshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
